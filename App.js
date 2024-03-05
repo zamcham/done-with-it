e0ce64527ed1191fafeca787a4236e9117025cfe
Cristian Zambrano <criszamcham@gmail.com>
2024-03-04 19:45:36 -0700

    Test touchable components

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  Image,
+  View,
+  SafeAreaView
+} from 'react-native';
 
 export default function App() {
+  const handlePress = () => console.log('Text pressed');
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text onPress={handlePress}>Hello World!</Text>
+      <TouchableOpacity onPress={() => console.log("Image tapped")}>
+        <Image
+          source={{
+            width: 200,
+            height: 300,
+            uri: 'https://picsum.photos/200/300',
+
+          }}
+        />
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +33,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
